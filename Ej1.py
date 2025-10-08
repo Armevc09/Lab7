290113f66ccf20d62534b30a33a989b996c44777
Armevc09 <kevin.calani@ucb.edu.bo>
2025-10-08 00:55:26 -0400

    Delete Ej1.py

--- Ej1.py
+++
@@ -1,7 +0,0 @@
-# 1. Cree un programa que abra una imagen y la redimensionamos a 1000x1000 pixeles
-#usando open cv
-import cv2
-imagen = cv2.imread('rob.png')
-imagen_redimensionada = cv2.resize(imagen, (1000, 1000))
-cv2.imwrite('imagen_redimensionada.jpg', imagen_redimensionada)
-
